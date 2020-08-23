af38e836082194e7c6c079ab0816ce8c53ce976a
sajjadhussains <sajjadhussain14.bd@gmail.com>
2020-08-23 21:33:03 +0600

    pass objects to components,one element

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products=[{name:'photoshop',price:'$90.99'}];
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           I am a react person
         </p>
+        <Product name={products[0].name}></Product>
+        
         <Person name='Rubel Nayok' nayika='Mousumi'></Person>
         <Person name='Jasim' nayika='Shabana'></Person>
         <Person name='Bapparaz'nayika='Cheka'></Person>
@@ -19,6 +24,24 @@ function App() {
   );
 }
 
+function Product(props){
+
+  const productStyle = {
+    border:'1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+
+  return(
+    <div style={productStyle}>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
 function Person(props){
   const personStyle={
     border:'4px solid cyan',
@@ -27,7 +50,7 @@ function Person(props){
   return (
   <div style={{personStyle}}>
     <h1>Nayok:{props.name}</h1>
-  <h3>Nayika:{props.nayika}</h3>
+  
   </div>
   )
 }
