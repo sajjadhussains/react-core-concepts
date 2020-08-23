03f6bb25e23fb9c027d3b3fec22af77b3dd6a90c
sajjadhussains <sajjadhussain14.bd@gmail.com>
2020-08-23 21:03:29 +0600

    pass dynamic data to Components by props

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit done <code>src/App.js</code> and save to reload.
+          I am a react person
         </p>
-        
+        <Person name='Rubel Nayok' nayika='Mousumi'></Person>
+        <Person name='Jasim' nayika='Shabana'></Person>
+        <Person name='Bapparaz'nayika='Cheka'></Person>
+        <Person name='Elias' nayika='Bobita'></Person>
+      
       </header>
     </div>
   );
+}
+
+function Person(props){
+  const personStyle={
+    border:'4px solid cyan',
+    margin:'10px'
+}
+  return (
+  <div style={{personStyle}}>
+    <h1>Nayok:{props.name}</h1>
+  <h3>Nayika:{props.nayika}</h3>
+  </div>
+  )
 }
 
 export default App;
