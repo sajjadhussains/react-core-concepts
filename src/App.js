d165983e309f81a120170e1702620ee4e9e15225
sajjadhussains <sajjadhussain14.bd@gmail.com>
2020-08-23 22:01:01 +0600

    pass object,,just some different way

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 function App() {
 
-  const products=[{name:'photoshop',price:'$90.99'}];
+  const products=[
+    {name:'photoshop',price:'$90.99'},
+    {name:'Illustrate',price:'60.99'},
+    {name:'PDF Reader',price:'$6.99'}
+  ];
 
   return (
     <div className="App">
@@ -12,7 +16,7 @@ function App() {
         <p>
           I am a react person
         </p>
-        <Product name={products[0].name}></Product>
+        <Product name={products[0].name} price={products[0].price}></Product>
         
         <Person name='Rubel Nayok' nayika='Mousumi'></Person>
         <Person name='Jasim' nayika='Shabana'></Person>
@@ -31,13 +35,14 @@ function Product(props){
     borderRadius:'5px',
     backgroundColor:'lightgray',
     height:'200px',
-    width:'200px',
+    width:'300px',
     float:'left'
   }
 
   return(
     <div style={productStyle}>
       <h1>{props.name}</h1>
+      <h1>{props.price}</h1>
     </div>
   )
 }
@@ -50,7 +55,7 @@ function Person(props){
   return (
   <div style={{personStyle}}>
     <h1>Nayok:{props.name}</h1>
-  
+    <h1>Nayika:{props.nayika}</h1>
   </div>
   )
 }
